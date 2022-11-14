56a4477a80e636552c8bc7a0556fdb8759273a43
David Ugalde <66655507+davidugalde11@users.noreply.github.com>
2022-11-14 18:10:53 -0500

    Update tree.py
    Added functionality to find trees by parts of their names by adding their "keys" to their dictionaries with the use of the re.split function to further specify each key with the white space and dash. Only need to finish part four by displaying number of trees per borough and do part 5.

--- tree.py
+++ tree.py
@@ -11,11 +11,17 @@ import sys
 import pandas as pd
 import numpy as np
 from collections import Counter
+import re
 
 # Check if there is only one argument given:
 if len(sys.argv) != 2:
     print('Usage: include only the tree.csv')
     sys.exit()
+# Check if the file given is in csv format:
+if not sys.argv[1].endswith('.csv'):
+    print("The file may not be in the correct format or be corrupted.")
+    sys.exit()
+
 
 # Try to create a data frame with Pandas:
 try:
@@ -58,14 +64,35 @@ try:
         trees[tree]['Coordinates'] = list(zip(x_list, y_list))
     # Use dictionary comprehension to convert keys to lower case:
     trees_final = {k.lower(): v for k, v in trees.items()}
+    # Create a new key value that contains the 'key' of tree:
+    for tree in trees_final.keys():
+        # Accomplish with re module that allows numerous delimiters, done for each tree:
+        trees_final[tree]["Keys"] = re.split("-| ", tree)
 
     # Define function to print tree info:
-    def tree_info(tree):
-        print(f"Total number of such trees: {trees_final[tree]['Total']}")
-        # Limit the number of zip codes to 5 for better visualization with list splicing:
-        print(f"Zip codes in which this tree is found: {trees_final[tree]['Zip Code'][:5]} and more.")
-        print(f"Borough containing the largest number of trees: {trees_final[tree]['Borough']}, with {trees[tree]['Borough Count']}")
+    def tree_info(query):
+        # Remove the first element which corresponds to command "treeinfo":
+        query.remove(query[0])
+        # Define matches variable as an empty list:
+        matches = []
+        for tree in trees_final.keys():
+            if trees_final[tree]["Keys"] == query:
+                print(f"Total number of such trees: {trees_final[tree]['Total']}")
+                # Limit the number of zip codes to 5 for better visualization with list splicing:
+                print(f"Zip codes in which this tree is found: {trees_final[tree]['Zip Code'][:5]} and more.")
+                print(f"Borough containing the largest number of trees: {trees_final[tree]['Borough']}, with {trees[tree]['Borough Count']}")
+                return
+        for key in query:
+            for tree in trees_final.keys():
+                if key in trees_final[tree]["Keys"]:
+                    matches.append(tree)
+        print("All matching species:")
+        for match in matches:
+            print(match + "\n")
 
+
+
+    # Define the most common function:
     def most_common():
         # Use list comprehension to create list of trees all lower values to prevent inconsistent data portrayal:
         tree_list_lower = [tree.lower() for tree in temp_tree]
@@ -73,37 +100,62 @@ try:
         most_common = tree_count.most_common(20)
         # Set number variable to add manual counter for better visualization:
         num = 1
+        # Print a caption for values:
+        print("Here are the twenty most popular trees in NYC:")
         for tree in most_common:
             print(f"{num}. {tree[0]}")
             num +=1
-            
-#treeinfo command
-if command == 'treeinfo'
-    #prompt to input tree species name
-    tree_species = input("Please select a tree species: ")
-    #list individual words in trees dictionary
-    newtreeslist = [i.split(' ')[0:-1] for i in list]
-    #if input matches full tree name, print tree info
-    for i in tree_list:
-        if tree_species in i:
-            print(tree_info)
-        elif:
-            #if input matches one or more words in tree substring, print tree info and all matching species
-            for i in newtreeslist:
-                if tree_species in i:
-                    print(tree_info)
-                    print(matching_species)
+
+    # Define the get help function:
+    def get_help():
+        print("These are the available commands in the treequery program:")
+        print("1. help")
+        print("2. listtrees")
+        print("3. treeinfo (with at least one input argument")
+        print("4. nearby (with some input arguments")
+        print("5. most_common")
+        print("6. quit")
+
+    # Define the list trees function:
+    def list_trees():
+        # Create temp list to modify without interfering with other functions:
+        temp_tree_list = [tree.lower() for tree in tree_list]
+        temp_tree_list.sort()
+        for tree in temp_tree_list:
+            print(tree)
+
+
+    # Set interactive loop so user can keep asking questions until program ends:
+    # Set a variable to true that can be changed to false when the loop is over:
+    is_on = True
+    # Welcome message:
+    print("Welcome to the treequery program")
+    print("To begin, try typing 'help' for the list of valid commands.")
+    while is_on:
+        # Get user input and reformat it to lowercase:
+        command = input("Enter a command: ").lower()
+        # Turn command into a list using re.split to also account for trees with "-" in their name.
+        command = re.split("-| ", command)
+        # Call appropriate function or response based on user input:
+        if command[0] == 'help':
+            get_help()
+        elif command[0] == 'listtrees':
+            list_trees()
+        # Input the list we made as the parameter for tree_info():
+        elif command[0] == 'treeinfo':
+            tree_info(command)
+        elif command[0] == 'nearby':
+            pass
+        elif command[0] == 'quit':
+            print("Thank you for using treequery program!")
+            is_on = False
+            sys.exit()
+        elif command[0] == 'commontrees':
+            most_common()
+        # Anything else the program can't account for is invalid:
         else:
-            continue
+            print("Invalid command. Type 'help' for the list of valid commands")
 
-#define matching species
-def matching_species():
-    for i in newtreeslist:
-        if tree_species in i:
-            name_match = tree_species.index(i)
-            print('All matching species: \n', tree_species[int(name_match)])
-
-#define popularity in the city
 
 # Catch exception if file passed doesn't exist:
 except FileNotFoundError:
@@ -113,6 +165,8 @@ except FileNotFoundError:
 except PermissionError:
     print("You don't have the appropriate permission to read this file")
     sys.exit()
+
+
 
 
 
